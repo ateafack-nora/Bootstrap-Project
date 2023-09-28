753b380021fb4eff8b04d6f402a8d10805890fc8
Ateafack Nora <64075437+ateafack-nora@users.noreply.github.com>
2023-09-28 22:01:18 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>bootstrapImpact</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  
+  </head>
+  <body>
+  <!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark sticky-top">
+        <div class="container-fluid"> 
+            <a class="navbar-brand" href="#">Impact<span style="color: red;">.</span></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Portfolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Team</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Blog</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Drop Down</a>
+                         <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Drop Down 1</a></li>
+                            <li><a class="dropdown-item" href="#">Drop Down 2</a></li>
+                            <li><a class="dropdown-item" href="#">Drop Down </a></li>
+                            <li><a class="dropdown-item" href="#">Drop Down 3</a></li>
+                            <li><a class="dropdown-item" href="#">Drop Down 4</a></li>
+                            <li><a class="dropdown-item" href="#">Drop Down 5</a></li>
+                         </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- section one -->
+
+<div class = "sectionone">
+    <div class="row align-items-center">
+        <div class="col-md-6 mx-auto">
+            <div class="text-center">
+                <h1 style="color: white;">Welcome To Impact</h1>
+                <p style="color: white; font-size: 100%"> Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br> Et, sint quas non unde ratione ea! Id aliquid.</p>
+                    <button type="button" class="btn btn-outline-success">Get Started</button>
+            </div>
+        </div>
+
+        <div class="col-md-6 mx-auto">
+            <img src="images/hero-img.SVG" class="mx-auto d-block"> 
+            
+        </div>      
+    </div>
+</div>
+
+
+<!-- ABOUT US -->
+<div class="about us">
+    <h1 style=" font-size: 200%; text-align :center;text-decoration: underline teal;text-decoration-style: solid;padding:25px;">About Us</h1>
+    <p style=" font-size: 100%; text-align: center;"> Lorem ipsum dolor sit amet, consectetur adipisicing elit.  consectetur adipisicing elit Id aliquid.</p>
+
+    <div class="row align-items-center">
+        <div class="col-md-6 mx-auto">
+            <div class= "aboutus1">
+                <h3 style="text-decoration-style: solid;">Lorem ipsum dolor sit amet, consectetur adipisicing elit .</h3>
+                <img src="images/about.jpg" class="rounded" class="mx-auto d-block" height="400" width="500">
+                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br> Et, sint quas non unde ratione ea! Id aliquid.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br> Et, sint quas non unde ratione ea! Id aliquid.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br> Et, sint quas non unde ratione ea! Id aliquid.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br> Et, sint quas non unde ratione ea! Id aliquid.
+                 </p>
+            </div>  
+        </div>
+        <div class="col-md-6 mx-auto">
+             <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br> Et, sint quas non unde ratione ea! Id aliquid.</P>
+             <div class="m-8">
+                    <ul>
+                        <li> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+                        <li> Lorem ipsum dolor sit amet, consectetur adipisicing .</li>
+                        <li> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipi. Lorem ipsum dolor sit amet, consectetur adipi.</li>
+                    </ul>                      
+             </div>
+             <p> aLorem ipsum dolor sit amet, consectetur adipisicing elit ratione ea! Id liquid..<br> Et, sint quas non unde ratione ea! Id liquid.<br>
+             aLorem ipsum dolor sit amet, consectetur adipisicing elit.<br> Et, sint quas non unde 
+            </P>
+            <img src="images/about-2.jpg" class="rounded" class="mx-auto d-block" height="400" width="500">  
+        </div>
+    <div>
+
+    <!-- slide images -->
+    <div class="container">
+        <div class="row align-items-center ">
+            <div class="col-md-1 custom-column">
+                <img src="images/client-1.png" alt="client-1" class="img-fluid">
+            </div>
+            <div class="col-md-1 custom-column">
+                <img src="images/client-3.png" alt="client-3" class="img-fluid">
+            </div>
+            <div class="col-md-1 custom-column">
+                <img src="images/client-4.png" alt="client-4" class="img-fluid">
+            </div>
+            <div class="col-md-1 custom-column">
+                <img src="images/client-6.png" alt="client-6" class="img-fluid">
+            </div>
+            <div class="col-md-1 custom-column">
+                <img src="images/client-7.png" alt="client-7" class="img-fluid">
+            </div>
+            <div class="col-md-1 custom-column">
+                <img src="images/client-8.png" alt="client-8" class="img-fluid">
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="boostrap/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
